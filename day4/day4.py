06a7600c22819219be105fbc1439fd6b2329930f
Maxime Andre <maxime.andre@cdiscount.com>
2024-12-04 11:29:23 +0100

    refacto

--- day4/day4.py
+++ day4/day4.py
@@ -1,6 +1,7 @@
-center_p1 = 'X'
-center_p2 = 'A'
-possibilities_p1 = [
+config = {}
+config[1] = {
+    'center': 'X',
+    'possibilities': [
         [{'x': -1, 'y':-1}, {'x': -2, 'y':-2}, {'x': -3, 'y':-3}],
         [{'x': -1, 'y':0}, {'x': -2, 'y':0}, {'x': -3, 'y':0}],
         [{'x': -1, 'y':1}, {'x': -2, 'y':2}, {'x': -3, 'y':3}],
@@ -9,39 +10,36 @@ possibilities_p1 = [
         [{'x': 1, 'y':0}, {'x': 2, 'y':0}, {'x': 3, 'y':0}],
         [{'x': 1, 'y':-1}, {'x': 2, 'y':-2}, {'x': 3, 'y':-3}],
         [{'x': 0, 'y':-1}, {'x': 0, 'y':-2}, {'x': 0, 'y':-3}]
-    ]
-possibilities_p2 = [
+    ],
+    'chain': 'MAS'
+}
+config[2] = {
+    'center': 'A',
+    'possibilities': [
         [{'x': -1, 'y':-1}, {'x': -1, 'y':1}, {'x': 1, 'y':1}, {'x': 1, 'y':-1}],
         [{'x': 1, 'y':-1}, {'x': -1, 'y':-1}, {'x': -1, 'y':1}, {'x': 1, 'y':1}],
         [{'x': 1, 'y':1}, {'x': 1, 'y':-1}, {'x': -1, 'y':-1}, {'x': -1, 'y':1}],
         [{'x': -1, 'y':1}, {'x': 1, 'y':1}, {'x': 1, 'y':-1}, {'x': -1, 'y':-1}]
-    ]
-chain_p1 = 'MAS'
-chain_p2 = 'MMSS'
+    ],
+    'chain': 'MMSS'
+}
 
 def compute(content, part=1):
     dict = {}
     for i in range(len(content)):
         for j in range(len(content[i])):
             dict[str(i) + ',' + str(j)] = content[i][j]
-    possibilities = possibilities_p1
-    chain = chain_p1
-    center = center_p1
-    if part == 2:
-        possibilities = possibilities_p2
-        chain = chain_p2
-        center = center_p2
 
     sum = 0
     for key in dict:
-        if dict[key] == center:
+        if dict[key] == config[part]['center']:
             x_i, x_j = int(key.split(',')[0]), int(key.split(',')[1])
-            for possibility in possibilities:
+            for possibility in config[part]['possibilities']:
                 result = ''
                 for p in possibility:
                     i, j = x_i + p['x'], x_j + p['y']
                     if (str(i) + ',' + str(j)) in dict.keys():
                         result += dict[str(i) + ',' + str(j)]
-                if result == chain:
+                if result == config[part]['chain']:
                     sum += 1
     return sum
